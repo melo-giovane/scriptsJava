41645e88598fdf541bdcd542d95f32008df879d3
Giovane <giovanec.melo@hotmail.com>
2025-10-20 22:02:17 -0300

    exemplo de estrutura condicional ternarios incluido

--- src/basico/Ternarios.java
+++ src/basico/Ternarios.java
@@ -1,4 +1,11 @@
 package basico;
 
+
 public class Ternarios {
+    public  static void main (String[] args){
+        double preco = 34.5;
+        double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
+
+        System.out.println("Desconto = " + desconto);
+    }
 }
