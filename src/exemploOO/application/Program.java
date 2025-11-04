113affbcca24c11900090792d8a1cd3a4ae06c25
Giovane Melo <giovanec.melo@hotmail.com>
2025-11-03 23:57:30 -0300

    exercicio 1 para exemplificar a necessidade de usar membros estáticos

--- src/exemploOO/application/Program.java
+++ src/exemploOO/application/Program.java
@@ -23,14 +23,14 @@ public class Program {
         System.out.println("Product data: " + product);
 
         System.out.println();
-        System.out.println("Enter the number of products to be added in stock: ");
+        System.out.print("Enter the number of products to be added in stock: ");
         int quantity = sc.nextInt();
         product.addProducts(quantity);
 
         System.out.println();
         System.out.println("Updated data: " + product);
         System.out.println();
-        System.out.println("Enter the number of products to be removed in stock: ");
+        System.out.print("Enter the number of products to be removed in stock: ");
         quantity = sc.nextInt();
         product.removeProducts(quantity);
         System.out.println("Updated data: " + product);
