96f15959cbd4c1ed317a6ca693e740159e7a6abf
Giovane Melo <giovanec.melo@hotmail.com>
2025-11-05 00:38:42 -0300

    exercicio 2 para exemplificar a necessidade de usar membros estáticos

--- src/basico/Ternarios.java
+++ src/basico/Ternarios.java
@@ -1,11 +1,10 @@
 package basico;
 
-
 public class Ternarios {
-    public  static void main (String[] args){
-        double preco = 34.5;
-        double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
+  public static void main(String[] args) {
+    double preco = 34.5;
+    double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
 
-        System.out.println("Desconto = " + desconto);
-    }
+    System.out.println("Desconto = " + desconto);
+  }
 }
