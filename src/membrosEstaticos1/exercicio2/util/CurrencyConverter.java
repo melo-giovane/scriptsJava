6420cefb59b7c2335174e8d75509b20284a2f5d7
Giovane Melo <giovanec.melo@hotmail.com>
2025-11-05 00:54:29 -0300

    Atualizaçao classe exemplo inicial para introduzir construtores

--- src/membrosEstaticos1/exercicio2/util/CurrencyConverter.java
+++ src/membrosEstaticos1/exercicio2/util/CurrencyConverter.java
@@ -3,7 +3,7 @@ package membrosEstaticos1.exercicio2.util;
 public class CurrencyConverter {
     public static double iof(double dollar){
         return dollar*0.06;
-    };
+    }
 
     public static double finalValue(double dollar, double price){
         return (dollar + iof(dollar)) * price;
